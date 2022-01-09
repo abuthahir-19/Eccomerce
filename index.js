69df8482a7c78743ffc1f892c98875194068b0b5
abuthahir-19 <abuthahirstar1@gmail.com>
2022-01-09 15:58:06 -0800

    added required files

---
+++ index.js
@@ -0,0 +1,33 @@
+var count = 0;
+var clicked = false;
+var updated;
+
+var x = document.getElementById ('quantity_value');
+var badge = document.getElementById ('badge_value');
+var plusButton = document.getElementById ('plus_btn');
+var minusButton = document.getElementById ('minus_btn');
+
+x.innerHTML = count;
+badge.innerHTML = count;
+
+function handleIncreament () {
+    count += 1;
+    updated = count;
+    badge.innerHTML = count;
+    x.innerHTML = count;
+}
+
+function handleDecreament () {
+    if (count !== 0) count -= 1;
+    badge.innerHTML = count;
+    x.innerHTML = count;
+}
+
+var cartBox = document.getElementById('cart-list');
+
+function displayDetails () {
+    if (!clicked)
+    cartBox.style.display = 'block';
+    else cartBox.style.display = 'none';
+    clicked = !clicked;
+}
