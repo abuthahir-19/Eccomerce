c7db795a8514815748f45a90e160090e9e38e551
abuthahir-19 <abuthahirstar1@gmail.com>
2022-01-09 22:16:41 -0800

    updated some changes

--- index.js
+++ index.js
@@ -7,19 +7,77 @@ var x = document.getElementById ('quantity_value');
 var badge = document.getElementById ('badge_value');
 var plusButton = document.getElementById ('plus_btn');
 var minusButton = document.getElementById ('minus_btn');
+var ItemCount = document.getElementById ('number_of_quantity');
+var emptyContent = document.getElementById ('item-count');
+var totalPrice = document.getElementById ('total_price');
+
+//cart box related variables
+var checkoutBtn = document.getElementById ('checkout_button');
+var cartBoxContent = document.getElementById ('cart-box');
+var productPhoto = document.getElementById ('product_photo');
+var itemDet = document.getElementById ('item-details');
+var delIcon = document.getElementById ('delete_icon');
+var whenEmpty = document.getElementById ('empty');
 
 x.innerHTML = count;
 badge.innerHTML = count;
+if (count == 0) {
+    badge.style.display = 'none';
+    productPhoto.style.display = 'none';
+    checkoutBtn.style.display = 'none';
+    itemDet.style.display = 'none';
+    delIcon.style.display = 'none';
+
+    whenEmpty.innerHTML = 'Your cart is empty.';
+    cartBoxContent.style.width = '100%';
+    cartBoxContent.style.height = '100%';
+}
 
 function handleIncreament () {
     count += 1;
+    if (count == 1) {
+        cartBoxContent.style.height = '80%';
+    }
+    if (count > 0) {
+        badge.style.display = 'flex';
+        badge.style.paddingTop = '2px';
+        ItemCount.innerHTML = count;
+
+        let tot = String(125 * count);
+        totalPrice.innerHTML = ' $' + tot.padEnd (tot.length + 3, '.00');
+
+        productPhoto.style.display = 'block';
+        checkoutBtn.style.display = 'block';
+        itemDet.style.display = 'block';
+        delIcon.style.display = 'block';
+        whenEmpty.innerHTML = '';
+    }
     updated = count;
     badge.innerHTML = count;
     x.innerHTML = count;
 }
 
 function handleDecreament () {
-    if (count !== 0) count -= 1;
+    if (count !== 0) {
+        count -= 1;
+        ItemCount.innerHTML = count;
+
+        let tot = String(125 * count);
+        totalPrice.innerHTML = ' $' + tot.padEnd (tot.length + 3, '.00');
+    }
+    if (count == 0) {
+        badge.style.display = 'none';
+        x.innerHTML = 0;
+
+        productPhoto.style.display = 'none';
+        checkoutBtn.style.display = 'none';
+        itemDet.style.display = 'none';
+        delIcon.style.display = 'none';
+
+        whenEmpty.innerHTML = 'Your cart is empty.';
+        cartBoxContent.style.width = '100%';
+        cartBoxContent.style.height = '100%';
+    }
     badge.innerHTML = count;
     x.innerHTML = count;
 }
@@ -33,6 +91,21 @@ function displayDetails () {
     clicked = !clicked;
 }
 
+function deleteItems () {
+    count = 0;
+    x.innerHTML = count;
+    badge.innerHTML = count;
+    badge.style.display = 'none';
+
+    productPhoto.style.display = 'none';
+    checkoutBtn.style.display = 'none';
+    itemDet.style.display = 'none';
+    delIcon.style.display = 'none';
+
+    whenEmpty.innerHTML = 'Your cart is empty.';
+    cartBoxContent.style.width = '100%';
+    cartBoxContent.style.height = '100%';
+}
 
 function toggleIcon () {
     document.getElementById('side-menu').style.transform = 'translate(0%)';
