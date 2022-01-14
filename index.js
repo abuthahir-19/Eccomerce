b0a4a57fc126a3236b0796735cf12bd4df9959d9
abuthahir-19 <abuthahirstar1@gmail.com>
2022-01-14 08:51:36 -0800

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var count = 0;
 var clicked = false;
 var menuIconClicked = false;
-var updated;
+var updated = 0;
 
 var x = document.getElementById ('quantity_value');
 var badge = document.getElementById ('badge_value');
@@ -33,66 +33,43 @@ if (count == 0) {
     cartBoxContent.style.height = '100%';
 }
 
+var screen = window.matchMedia ("(max-width: 550px)");
+if (screen.matches) {
+    document.getElementById ('main_front_image').style.cursor = 'default';
+}
+
 function handleIncreament () {
     count += 1;
-    // if (count == 1) {
-    //     cartBoxContent.style.height = '80%';
-    // }
-    if (count > 0) {
-        // badge.style.display = 'flex';
-        // badge.style.paddingTop = '2px';
-        // ItemCount.innerHTML = count;
-
-        // let tot = String(125 * count);
-        // totalPrice.innerHTML = ' $' + tot.padEnd (tot.length + 3, '.00');
-
-        // productPhoto.style.display = 'block';
-        // checkoutBtn.style.display = 'block';
-        // itemDet.style.display = 'block';
-        // delIcon.style.display = 'block';
-        // whenEmpty.innerHTML = '';
-    }
-    updated = count;
-    // badge.innerHTML = count;
+    updated += count;
     x.innerHTML = count;
 }
 
 function handleDecreament () {
     if (count !== 0) {
         count -= 1;
-        // ItemCount.innerHTML = count;
-
-        // let tot = String(125 * count);
-        // totalPrice.innerHTML = ' $' + tot.padEnd (tot.length + 3, '.00');
     }
     if (count == 0) {
         badge.style.display = 'none';
         x.innerHTML = 0;
-
-        // productPhoto.style.display = 'none';
-        // checkoutBtn.style.display = 'none';
-        // itemDet.style.display = 'none';
-        // delIcon.style.display = 'none';
-
-        // whenEmpty.innerHTML = 'Your cart is empty.';
-        // cartBoxContent.style.width = '100%';
-        // cartBoxContent.style.height = '100%';
     }
-    // badge.innerHTML = count;
+    updated += count;
     x.innerHTML = count;
 }
 
 var cartBox = document.getElementById('cart-list');
 
-function displayDetails () {
+function displayCartDetails () {
     if (!clicked)
     cartBox.style.display = 'block';
-    else cartBox.style.display = 'none';
+    else {
+        cartBox.style.display = 'none';
+    }
     clicked = !clicked;
 }
 
 function deleteItems () {
     count = 0;
+    updated = 0
     x.innerHTML = count;
     badge.innerHTML = count;
     badge.style.display = 'none';
@@ -139,20 +116,20 @@ function addToCart () {
 
 //image rendering section
 
-var element = document.getElementById ('image-section');
-var imageParent = document.getElementById ('image-details');
+var element = document.getElementById ('main_front_image');
+var imageParent = document.getElementById ('main_image');
 
 function renderAsMainImage (id, active) {
     var imgTag;
-    if (active == undefined) {
+    if (!active) {
         imgTag = document.getElementById (id).getAttribute('src');
         imgTag = imgTag.slice (0, imgTag.indexOf('-thumbnail')) + '.jpg';
-        element.firstElementChild.firstElementChild.setAttribute('src', imgTag);
+        element.setAttribute('src', imgTag);
     }
     else if (active) {
         imgTag = document.getElementById (id).getAttribute('src');
         imgTag = imgTag.slice (0, imgTag.indexOf('-thumbnail')) + '.jpg';
-        imageParent.firstElementChild.nextElementSibling.firstElementChild.setAttribute ('src', imgTag);
+        imageParent.setAttribute ('src', imgTag);
     }
 }
 
@@ -165,4 +142,45 @@ function displayImageDetails () {
 
 function closeImageDetailsInfo() {
     document.getElementById('image-wrapper').style.display = 'none';
+}
+
+//Image sliding section code
+
+var imageCount = 0;
+var images = ['images/image-product-1.jpg', 'images/image-product-2.jpg', 'images/image-product-3.jpg', 'images/image-product-4.jpg']
+
+function showPrevImage (bool) {
+    if(bool) {
+        if (imageCount != 0) {
+            imageCount -= 1;
+            imageParent.setAttribute ('src', images[imageCount]);
+        }
+        else if (imageCount == 0) {
+            imageParent.setAttribute ('src', images[imageCount]);
+        }
+    }
+    else {
+        if (imageCount != 0) {
+            imageCount -= 1;
+            element.setAttribute ('src', images[imageCount]);
+        }
+        else if (imageCount == 0) {
+            element.setAttribute ('src', images[imageCount]);
+        }
+    }
+}
+
+function showNextImage (bool) {
+    if (bool) {
+        if (imageCount != images.length-1) {
+            imageCount += 1;
+            imageParent.setAttribute ('src', images[imageCount]);
+        }
+    }
+    else {
+        if (imageCount != images.length-1) {
+            imageCount += 1;
+            element.setAttribute ('src', images[imageCount]);
+        }
+    }
 }
